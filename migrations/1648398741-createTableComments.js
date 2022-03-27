2dbcbe1d9821de2aabfc679609da4c55796ad9b9
Tobyastro123 <tobyastro123@outlook.com>
2022-03-27 18:34:35 +0200

    Created new migration table for comments

---
+++ migrations/1648398741-createTableComments.js
@@ -0,0 +1,17 @@
+exports.up = async (sql) => {
+  await sql`
+  CREATE TABLE comments (
+		id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		comment varchar (600) NOT NULL,
+		user_id integer REFERENCES users (id) ON DELETE CASCADE,
+		username varchar(100) REFERENCES users (username) ON DELETE CASCADE,
+		post_id integer REFERENCES blogPosts (id) ON DELETE CASCADE
+	);
+	`;
+};
+
+exports.down = async (sql) => {
+  await sql`
+    DROP TABLE comments
+  `;
+};
